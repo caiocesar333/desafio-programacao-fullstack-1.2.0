0ce42b743dd996eec23d3ce17619159af589051c
Caio <caiocesar333@outlook.com>
2023-04-25 18:57:08 -0300

    Feat: table organization, front and backend connection

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,8 +1,12 @@
 
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import TransactionsTable from './components/TransactionsTable';
 
 const App = () => {
-    const [fileContent, setFileContent] = React.useState('');
+    const [data, setData] = useState<any[]>([]);
+
+    const [fileContent, setFileContent] = useState('');
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files && event.target.files[0];
@@ -15,12 +19,50 @@ const App = () => {
         }
     };
 
+    useEffect(() => {
+        fetch('http://localhost:8000/sales')
+            .then((res) => res.json())
+            .then((data) => setData(data))
+    }, [])
+
     return (
-        <form className=''>
-            <input type="file" onChange={handleFileChange} />
-            <p className=''>File content:</p>
-            <pre>{fileContent}</pre>
-        </form>
+        <div className='flex items-center justify-center flex-col w-full  h-full gap-11 '>
+            <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full text-left text-sm font-light">
+                                <thead className="border-b font-medium dark:border-neutral-500">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-4">Type</th>
+                                        <th scope="col" className="px-6 py-4">Date</th>
+                                        <th scope="col" className="px-6 py-4">Product Description</th>
+                                        <th scope="col" className="px-6 py-4">Value </th>
+                                        <th scope="col" className="px-6 py-4">Seller </th>
+                                    </tr>
+                                </thead>
+                                {data.map((item) => (
+                                    <TransactionsTable
+                                        key={item.id}
+                                        date={item.transactionDate}
+                                        sellerName={item.seller}
+                                        productDescription={item.productDesc}
+                                        value={item.transactionValue}
+                                        type={item.transactionType}
+                                    />
+                                ))}</table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <form className=''>
+                <input type="file" onChange={handleFileChange} />
+                <p className=''>File content:</p>
+                <pre>{fileContent}</pre>
+            </form>
+        </div>
+
     );
 };
 
---
+++ frontend/react-ts/src/components/TransactionsTable.tsx
@@ -0,0 +1,28 @@
+export default TransactionsTable
+
+type Transaction = {
+    type?: string;
+    date?: string;
+    productDescription?: string;
+    value?: number;
+    sellerName?: string;
+};
+
+function TransactionsTable({ type, date, productDescription, value, sellerName }: Transaction) {
+    return (
+
+        <tbody>
+            <tr
+                className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                <td className="whitespace-nowrap px-6 py-4 font-medium">{type}</td>
+                <td className="whitespace-nowrap px-6 py-4">{date}</td>
+                <td className="whitespace-nowrap px-6 py-4">{productDescription}</td>
+                <td className="whitespace-nowrap px-6 py-4">{value}</td>
+                <td className="whitespace-nowrap px-6 py-4">{sellerName}</td>
+            </tr>
+
+        </tbody>
+    )
+
+}
+
