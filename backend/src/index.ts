cd2cd50191878b0be81ba3dc2a749b87223efd6b
Caio <caiocesar333@outlook.com>
2023-04-25 12:08:43 -0300

    feat: add express initial server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log("olá")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(` listening on ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('olá')
+
+})
